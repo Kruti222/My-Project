fd6f6d47b0bc77d528c4b9fa2ebabc34acee7908
Kruti Patel <patelkruti2202@gmail.com>
2023-07-11 21:23:41 -0700

    first maven folder added

---
+++ my-assigment/src/test/java/com/assignment/my_assigment/DummyTest.java
@@ -0,0 +1,11 @@
+package com.assignment.my_assigment;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DummyTest {
+    @Test
+    public void testDummy() {
+        assertTrue(true);
+    }
+}
